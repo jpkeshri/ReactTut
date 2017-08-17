53d19d86794bccf3615bf54ede2d766274c099bd
Jai prakash <jpk1208@gmail.com>
2017-08-18 01:30:55 +0530

    add image

--- components/Profile.js
+++ components/Profile.js
@@ -13,33 +13,36 @@ export default class Profile extends React.Component {
     <div class="row">
         <div className="col-md-4">
             <div className="thumbnail">
-                <a href="image/boats_painted_x.jpg" target="_blank"/>
-                    <img  className="img-responsive" src="..public/boats_painted_x.jpg" alt="idjhjh" >
+                <a href="image/boats_painted_x.jpg" target="_blank">
+                    <img className="img-responsive" src="../public/profile.jpg" alt="idjhjh" />
                         <div className="caption"> <p> this  is first image of gallery</p>
                         </div>
-                               </a>
-
-            </div>
-        </div>
-                <div className="row">
+                        </a>
+                  </div>
+                           
+           </div>
+        
+                
         <div className="col-md-4">
             <div className="thumbnail">
-                <a href="image/beautiful_forest_waterfall_2.jpg" target="_blank"/>
-                    <img  className="img-responsive" src="..public/beautiful_forest_waterfall_2.jpg" alt="idjhjh" >
+                <a href="image/beautiful_forest_waterfall_2.jpg" target="_blank">
+                    <img className="img-responsive" src="../public/beautiful_forest_waterfall_2.jpg" alt="idjhjh" />
                         <div className="caption"> <p> this  is first image of gallery</p>
                         </div>
-                </a>
-            </div>
+                        </a>
+                    </div>
+            
         </div>
-                <div className="row">
+            
         <div className="col-md-4">
             <div className="thumbnail">
-                <a href="image/forest_lake_2.jpg"" target="_blank">
-                    <img  className="img-responsive" src="..public/forest_lake_2.jpg" alt="idjhjh" />
+                <a href="image/forest_lake_2.jpg" target="_blank">
+                    <img className="img-responsive" src="../public/forest_lake_2.jpg" alt="idjhjh" />
                         <div className="caption"> <p> this  is first image of gallery</p>
                         </div>
-                </a>
-            </div>
+                        </a>
+                </div>
+            
         </div>
      </div> 
 			</div>
