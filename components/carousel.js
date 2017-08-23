8d53030d7d211bb9c0cc56ae2d559af91909a026
Jai prakash <jpk1208@gmail.com>
2017-08-23 12:17:53 +0530

    add carousel

--- components/carousel.js
+++ components/carousel.js
@@ -21,51 +21,51 @@ export default class carausel extends react.component(){
 </ol>
 <div className="carousel-inner">
 <div classname="item active">
-<img className="responsive" src="image/smurfs_2017.jpg" alt="image/smurfs_2017.jpg" style="width:100%;height:600px" >
+<img className="responsive" src="..public/smurfs_2017.jpg" alt="image/smurfs_2017.jpg" style="width:100%;height:600px" />
 <div className="carousel-caption">
     <h4>natural pic</h4>
  </div>
     </div>
     
 <div className="item">
-<img  className="responsive"src="image/small_river_near_house.jpg"  alt="image/small_river_near_house.jpg"  style="width:100%;height:600px" >
+<img  className="responsive" src="..public/small_river_near_house.jpg"  alt="..public/small_river_near_house.jpg"  style="width:100%;height:600px" / >
 <div className="carousel-caption">
 <h4>natural pic</h4>
-    </div>
+>>    </div>
     </div>
 
 <div className="item">
-<img className="responsive"src="image/luxurious_villa_on_the_beach-852x480.jpg" alt="image/luxurious_villa_on_the_beach-852x480.jpg"  style="width:100%;height:600px" >
+<img className="responsive" src="..public/luxurious_villa_on_the_beach-852x480.jpg" alt="image/luxurious_villa_on_the_beach-852x480.jpg"  style="width:100%;height:600px" />
 <div className="carousel-caption">
     <h4>natural pic</h4>
 </div>
 </div>
 <div className="item">
-<img className="responsive" src="image/house_on_the_green_field.jpg" alt="image/house_on_the_green_field.jpg"  style="width:100%;height:600px" >
+<img className="responsive" src="..public/house_on_the_green_field.jpg" alt="image/house_on_the_green_field.jpg"  style="width:100%;height:600px" / >
 <div className="carousel-caption">
 <h4>natural pic</h4>
 </div>
 </div>
 <div className="item">
-<img  className="responsive" src="image/forest_lake_2.jpg" alt="image/forest_lake_2.jpg"  style="width:100%;height:600px" >
+<img  className="responsive" src="..public/forest_lake_2.jpg" alt="image/forest_lake_2.jpg"  style="width:100%;height:600px" />
 <div className="carousel-caption">
     <h4>natural pic</h4>
 </div>
 </div>
 <div className="item">
-<img className="responsive" src="image/girl_on_the_beach.jpg" alt="image/girl_on_the_beach.jpg"  style="width:100%;height:600px" >
+<img className="responsive" src="..public/girl_on_the_beach.jpg" alt="image/girl_on_the_beach.jpg"  style="width:100%;height:600px" />
 <div className="carousel-caption">
 <h4>natural pic</h4>
 </div>
 </div>
    <div className="item">
-<img className="responsive" src="image/boats_painted_x.jpg" alt="image/boats_painted_x.jpg"  style="width:100%;height:600px" >
+<img className="responsive" src="..public/boats_painted_x.jpg" alt="image/boats_painted_x.jpg"  style="width:100%;height:600px" / >
 <div className="carousel-caption">
     <h4>natural pic</h4>
 </div>
 </div>
 <div className="item">
-<img className="responsive" src="image/purple_sky_scene.jpg" alt="image/purple_sky_scene.jpg"  style="width:100%;height:600px" >
+<img className="responsive" src="..public/purple_sky_scene.jpg" alt="image/purple_sky_scene.jpg"  style="width:100%;height:600px" />
 <div className="carousel-caption">
 <h4>natural pic</h4>
 </div>
